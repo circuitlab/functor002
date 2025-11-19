85a448e52fb1ffa64ab6742bdfb1d6e07b7d826b
Haruma Kikuchi <haruma@uniba.jp>
2025-11-19 09:30:18 +0900

    Fix

--- src/pigeon-receiver.js
+++ src/pigeon-receiver.js
@@ -2,14 +2,14 @@ import { Pigeon } from "./Pigeon.js";
 
 AFRAME.registerComponent( "pigeon-receiver", {
   init() {
-    this.pigeon = new Pigeon( "wss://pigeon-room-dev.deno.dev/pigeon/", 'functor' );
+    this.pigeon = new Pigeon( "wss://vmb207.circuitlab.team:3001/pigeon/", 'functor' );
 
     document.addEventListener( "ipad", ( e ) => {
       console.log( e.detail );
 
       const quaternion = new THREE.Quaternion( e.detail.x, e.detail.y, e.detail.z, e.detail.w );
 
-      this.el.object3D.applyQuaternion( quaternion );
+      this.el.object3D.setRotationFromQuaternion( quaternion );
     } );
   },
   tick() {
